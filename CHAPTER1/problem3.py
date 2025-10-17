429838f3c5b64b38115410fc960ecf6597756435
Mohammad Zakir Nizam <jakir.nizam@niveussolutions.com>
2025-10-17 13:55:48 +0530

    List the content of a directory using OS Module (Chapter 1)

--- CHAPTER1/problem3.py
+++ CHAPTER1/problem3.py
@@ -1,5 +1,12 @@
 # reading a files using OS Module
+#import OS Module
 import os
+
+# Select the directory whose content you want to list
 directory_path ="/home/niveus/NizamPersonal"
+
+# Use the os module to list the directory content
 contents = os.listdir(directory_path)
+
+#Print the contents of the directory
 print(contents)
